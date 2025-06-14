6dd5a97d3159a04ee88352e78ec0c88f380c54d6
TheRamza <ramza@machlink.com>
2025-06-14 14:47:23 -0500

    Update: added player with turning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 run_game = True
 
 def main():
@@ -7,15 +10,21 @@ def main():
     screen_fps = pygame.time.Clock()
     dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while run_game == True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+            
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        # FPS Control
         dt = screen_fps.tick(60) / 1000
-        print(dt)
 
 
 
