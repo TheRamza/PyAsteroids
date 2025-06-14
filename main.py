cadb3f290b742c4e1c7e815b821f29f3f84a69ad
TheRamza <ramza@machlink.com>
2025-06-14 15:38:15 -0500

    Update: player now in groups, updatable and drawable, using updatable for updates

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     screen_fps = pygame.time.Clock()
     dt = 0 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while run_game == True:
@@ -20,7 +24,7 @@ def main():
             
         screen.fill("black")
         player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
         pygame.display.flip()
 
         # FPS Control
